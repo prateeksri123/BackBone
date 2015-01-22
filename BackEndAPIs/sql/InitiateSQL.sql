5d48d296759f6325848b83bec493109bf9f0e686
prateeksri123 <me.prateek.srivastava@gmail.com>
2015-01-22 12:46:43 +0530

    SQL script updation

--- BackEndAPIs/sql/InitiateSQL.sql
+++ BackEndAPIs/sql/InitiateSQL.sql
@@ -1,19 +1,28 @@
-CREATE TABLE `wishlist`.`Users` (
-  `UserName` VARCHAR(255) NOT NULL,
-  `Password` VARCHAR(45) NOT NULL,
-  `userId` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  `FirstName` VARCHAR(255) NOT NULL,
-  `LastName` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`userId`)
-)
-CREATE TABLE `wishlist`.`Product_Category` (
-  `category_name` VARCHAR(255) NOT NULL,
-  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-)
-DROP TABLE IF EXISTS `wishlist`.`product`;
-CREATE TABLE  `wishlist`.`product` (
+-- MySQL dump 10.13  Distrib 5.5.41, for Win32 (x86)
+--
+-- Host: localhost    Database: wishlist
+-- ------------------------------------------------------
+-- Server version	5.5.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
   `id` varchar(255) NOT NULL,
   `product_title` varchar(255) NOT NULL,
   `product_description` longtext,
@@ -27,6 +36,48 @@ CREATE TABLE  `wishlist`.`product` (
   `category_id` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-ALTER TABLE `wishlist`.`product_category` ADD COLUMN `category_url` VARCHAR(255) NOT NULL AFTER `id`;
-ALTER TABLE `wishlist`.`product_category` MODIFY COLUMN `category_url` TEXT NOT NULL;
-ALTER TABLE `wishlist`.`product_category` MODIFY COLUMN `id` VARCHAR(255) NOT NULL;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `product_category`
+--
+
+DROP TABLE IF EXISTS `product_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_category` (
+  `category_name` varchar(255) NOT NULL,
+  `id` varchar(255) NOT NULL,
+  `category_url` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `UserName` varchar(255) NOT NULL,
+  `Password` varchar(45) NOT NULL,
+  `userId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-01-22 12:44:52
